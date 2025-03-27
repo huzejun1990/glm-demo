e3ad63684573af8b66c569dfa07ff4fe110042a8
huzejun <961639130@qq.com>
2025-03-27 20:41:30 +0800

    AI大模型开发（智谱）GLM4 开发环境、调用方式 2025-3-27 20:41:24

---
+++ demo1.py
@@ -0,0 +1,26 @@
+# @Author : huzejun
+# @Time : 2025/3/26 19:20
+from zhipuai import ZhipuAI
+
+# 使用GLM自己的
+
+# api_key = os.getenv('API_KEY')
+# print(api_key)
+client = ZhipuAI(api_key='4145705edf7f4dc0990ad60d4b5298ad.qVVdi0QBsBVOPVyy')
+response = client.chat.completions.create(
+    model='glm-4-0520', # glm-4-9b
+    # model='glm-4-flash',
+    messages=[
+        {'role': "user", 'content': '请介绍一下大模型的定义?'}
+    ],
+    stream=True
+)
+
+# 流式的输出
+for s in response:
+    print(s.choices[0].delta)
+
+# print(response)
+
+
+# print(response.choices[0].message.content)
---
+++ demo2.py
@@ -0,0 +1,33 @@
+# @Author : huzejun
+# @Time : 2025/3/26 20:14
+from openai import OpenAI
+# import os
+
+# 使用OpenAI自己的
+
+# api_key = os.getenv('API_KEY')
+# print(api_key)
+client = OpenAI(
+    api_key='4145705edf7f4dc0990ad60d4b5298ad.qVVdi0QBsBVOPVyy',
+    base_url='https://open.bigmodel.cn/api/paas/v4/'
+
+)
+
+# response = client.chat.completions.create(
+response = client.chat.completions.create(
+    # model='glm-4-0520', # glm-4-9b
+    model='glm-4-9b',
+    messages=[
+        {'role': "user", 'content': '请介绍一下大模型的定义?'}
+    ],
+    # stream=True
+)
+
+# 流式的输出
+# for s in response:
+#     print(s.chioces[0].delta)
+
+
+print(response)
+#
+print(response.choices[0].message.content)
